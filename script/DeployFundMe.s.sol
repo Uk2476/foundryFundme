63dabf9bc87c218eda932b36c65f38f99519fb0a
Uk2476 <kuttkarsh028@gmail.com>
2026-02-28 21:37:29 +0530

    deploy file

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identfier:MIT
+pragma solidity ^0.8.18;
+
+import {FundMe} from "../src/FundMe.sol";
+import {Script} from "forge-std/Script.so";
+
+contract DeployFundMe is Script {
+    Fundme fundMe ;
+
+    function run()
+}
