d561209239dc602a1a84e97ccba5b7d9f71c67a2
Uk2476 <kuttkarsh028@gmail.com>
2026-03-01 00:54:53 +0530

    code to access other networks

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+import {FundMe} from "src/FundMe.sol" ;
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+
+    address public networkUsed;
+
+    constructor () {
+        if (block.chainId == 11155111){
+            networkUsed = sepoliaId();
+        } else
+    }
+
+    function sepoliaId() public pure returns(address){
+        return 0x694AA1769357215DE4FAC081bf1f309aDC325306 ;
+    }
+
+    function anvilId() public pure returns {}
+}
+
+
+
+
